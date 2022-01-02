59c4f6352ca38163d264984cb552fa0efe7659cc
Vasant <vasant.chavan223@gmail.com>
2022-01-02 10:12:08 +0530

    object creation statement updated

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -11,7 +11,7 @@ public class Test {
 	}
 	public static void main(String[] args) {
 		
-		Test t = new Test
+		Test t = new Test();
 		int add = t.add(10,20);
 		System.out.println(add);
 		System.out.println(t.a+t.b);
