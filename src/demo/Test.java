7717504e29d1d5a81c353b85252c5579dc22a9b0
Vasant Chavan <51292423+VasantChavan@users.noreply.github.com>
2022-01-02 10:09:59 +0530

    Update Test.java

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -1,11 +1,20 @@
 package demo;
 
 public class Test {
-
+	int a;
+	int b;
 	
+	public int add(int a, int b){
+		this.a=a;
+		this.b=b;
+		return (a+b);
+	}
 	public static void main(String[] args) {
 		
+		Test t = new Test
+		int add = t.add(10,20);
+		System.out.println(add);
+		System.out.println(t.a+t.b);
 		
-		System.out.println("Hello guys , how are you");
 	}
 }
